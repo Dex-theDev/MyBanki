4e7fea73392a92bff3a9228e4f21a815cbc4a410
Jaime <jaime.joseph1991@gmail.com>
2022-06-19 22:14:16 -0400

    first commit

---
+++ public/add-edit-delete.js
@@ -0,0 +1,96 @@
+//const { double } = require("webidl-conversions")
+
+const deleteText = document.querySelectorAll('.garbageCan')
+//test//
+
+
+const updateButton = document.querySelector('#updateButton')
+const deleteButton = document.querySelector('#deleteButton')
+//updateButton.addEventListener('click', updateEntry)
+//deleteButton.addEventListener('click', deleteEntry)
+console.log('we here')
+
+//Create an array of all the current trash buttons on the page
+Array.from(deleteText).forEach(e => {
+    e.addEventListener('click', deleteQuestion)
+})
+
+console.log(Array.from(document.querySelectorAll('.questionCard')))
+
+// async function updateEntry(){
+//     try{
+//         const response = await fetch('updateEntry', {
+//             method: 'put',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//                 topic: document.getElementsByName("topic")[0].value,
+//                 question: document.getElementsByName("question")[0].value,
+//                 answer: document.getElementsByName("answer")[0].value
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     } catch(err){
+//         console.log(err)
+//     }
+
+// }
+
+// async function deleteEntry(){
+//     const input = document.getElementById('deleteInput')
+//     console.log(input.value)
+//     try{
+//         const response = await fetch('deleteEntry', {
+//             method: 'delete',
+//             headers: { 'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//                 name: input.value
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     } catch(err){
+//         console.error(err)
+//     }
+// }
+
+async function deleteQuestion(){
+   // console.log('button clicked')
+    console.log(this.parentNode)
+    const topic = this.parentNode.childNodes[1].innerText
+    const singleQuestion = this.parentNode.childNodes[3].innerText
+    const answer = this.parentNode.childNodes[5].innerText
+    try {
+        const response = await fetch('/deleteQuestion',{
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'topic': topic,
+                'question': singleQuestion,
+                'answer': answer
+            })
+
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err){
+        console.error(err)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,93 @@
+//Require dependancies
+const express = require('express')
+const PORT = 3000
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'banki-questions'
+
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Connected to ${dbName} database`)
+        db = client.db(dbName)
+      
+    })
+//parse requests of content-type
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+//allows you to render data to an ejs template
+app.use(express.static('public'))
+//allows for css and client side js to render on template
+app.use(express.urlencoded({ extended: true }))
+//middleware for parsing bodies from URL
+app.use(express.json())
+//parses incoming JSON requests and puts the parsed data in req.body
+
+
+app.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+app.get('/add-edit-delete', (req,res) => {
+    const cursor = db.collection('questions').find().toArray()
+    .then(results => {
+        res.render('add-edit-delete', { questions: results})
+    })
+    .catch(error => console.error(error))
+    //will add more later
+})
+
+app.post('/api', (req,res) => {
+    console.log('post heard')
+    db.collection('questions').insertOne(
+        req.body
+    )
+    .then(result => {
+        console.log(result)
+        console.log(req.body)
+       res.redirect('/')//redirecting to the same page for now
+      
+    })
+    .catch(error => console.error(error))
+})
+
+// put requests
+app.put('/updateEntry', (req,res) => {
+    console.log(req.body)
+    //converts the request body into an array and checks for falsy values with a forEach loop, if it's empty, delete it
+    Object.keys(req.body).forEach(key => {
+        if(req.body[key] === null || req.body[key] === undefined || req.body[key] === ''){
+            delete req.body[key]
+        }
+    })
+    console.log(req.body)
+    db.collection('questions').findOneAndUpdate(
+        //query,update,options
+        { question: req.body.question },
+        {
+            $set: req.body
+        }
+    )
+    .then(result => {
+        console.log(result)
+        res.json('success')
+    })
+    .catch(error => console.error(error))
+})
+
+app.delete('/deleteQuestion', (req,res) => {
+    db.collection('questions').deleteOne( {topic: req.body.topic, question: req.body.question, answer: req.body.answer} )
+    .then(result => {
+        console.log('Entry deleted')
+        res.json('Entry Deleted')
+    })
+    .catch(err => console.error(err))
+})
+app.listen(process.env.PORT || PORT, () => console.log(`Listening on port ${PORT}`))
